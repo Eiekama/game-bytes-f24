3053115b1bad28205fe01ed7351e42f372d211cf
cmoemoe <130932961+cmoemoe@users.noreply.github.com>
2024-10-06 15:53:35 -0400

    Two-Player Control System
    Movement and Throwing for Player 1 and Player 2
    
    Vertical, Horizontal, Jump, Run, Throw

--- Crucible/Assets/Minigames/DFrag/Scripts/Player1Movement.cs
+++ Crucible/Assets/Minigames/DFrag/Scripts/Player1Movement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerMovement : MonoBehaviour
+public class Player1Movement : MonoBehaviour
 {
     public Camera playerCamera;
     public float walkSpeed = 6f;
@@ -34,13 +34,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        bool isRunning = Input.GetButton("P1_Button2");
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("P1_Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("P1_Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        if (Input.GetButton("P1_Button1") && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
         }
@@ -54,13 +54,13 @@ public class PlayerMovement : MonoBehaviour
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.R) && canMove)
-        {
-            characterController.height = crouchHeight;
-            walkSpeed = crouchSpeed;
-            runSpeed = crouchSpeed;
+        // if (Input.GetButton("P1_Button2") && canMove)
+        // {
+        //     characterController.height = crouchHeight;
+        //     walkSpeed = crouchSpeed;
+        //     runSpeed = crouchSpeed;
 
-        }
+        // }
         else
         {
             characterController.height = defaultHeight;
