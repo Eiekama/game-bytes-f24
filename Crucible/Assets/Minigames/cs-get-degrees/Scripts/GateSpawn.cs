ddd33c599ed13f08b961256bcff9fef1b262fa91
Jacobn99 <167144778+Jacobn99@users.noreply.github.com>
2024-10-15 22:38:57 -0400

    Added literally like nothing
    fffff

--- Crucible/Assets/Minigames/cs-get-degrees/Scripts/GateSpawn.cs
+++ Crucible/Assets/Minigames/cs-get-degrees/Scripts/GateSpawn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GateSpawn : MonoBehaviour
@@ -7,13 +8,16 @@ public class GateSpawn : MonoBehaviour
     [SerializeField] GameObject fullGate;
 
     [SerializeField] GameObject playerOneBarriers;
-
     [SerializeField] float speed;
-  
+    [SerializeField] List<Transform> startLocs;
+    Dictionary<GameObject, gateObject> gates;
 
     // Start is called before the first frame update
     void Start()
     {
+        gates = new Dictionary<GameObject, gateObject>();
+        startLocs = new List<Transform>();
+
         Instantiate(fullGate, playerOneBarriers.transform);
         StartCoroutine(makeGate());
         
@@ -31,7 +35,20 @@ public class GateSpawn : MonoBehaviour
 
     IEnumerator makeGate()
     {
-        Instantiate(fullGate, playerOneBarriers.transform);
+        //Debug.Log(startLocs.ElementAt(0).transform.position);
+        GameObject gate = Instantiate(fullGate, playerOneBarriers.transform);
+        gateObject gate_object = GateDataController.getGateData(1);
+        gates.Add(gate, gate_object);
+        //foreach (Transform t in  startLocs)
+        //{
+        //    GameObject gate = Instantiate(fullGate, t);
+        //    gateObject gate_object = GateDataController.getGateData(1);
+        //    gates.Add(gate, gate_object);
+
+        //    //Instantiate(fullGate, playerOneBarriers.transform);
+        //}
+
+        //Instantiate(fullGate, startLocs.ElementAt(0));
         yield return new WaitForSeconds(2);
         StartCoroutine(makeGate());
     }
