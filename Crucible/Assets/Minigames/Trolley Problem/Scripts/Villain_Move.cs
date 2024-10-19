11d75b23c9f79cd75929ac639ce9cdfa11719e75
cnk2024 <chloenahlin@gmail.com>
2024-10-19 12:55:33 -0400

    Basic Villain Movement + Trolley Speed Adjustment
    Made the trolley go backwards slower
    
    Made the villain be able to move around
    
    Made the tracks they were on static (global) for collision purposes later

--- Crucible/Assets/Minigames/Trolley Problem/Scripts/Villain_Move.cs
+++ Crucible/Assets/Minigames/Trolley Problem/Scripts/Villain_Move.cs
@@ -2,67 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trolley_Move : MonoBehaviour
+public class Villain_Move : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    // public float moveIncrement = 2; 
-    public bool isMoving = false; //makes sure trolley can only switch tracks when its not doing so already (avoid switching multiple tracks at once, messes stuff up)
     private float exp = 1.1f; //some exponential speed to make it feel better
     public float forward_speed; //base speed 
-    public bool isSpeeding = false; //makes sure that the trolley can only speed up when its not already speeding (avoid super fast trolley bug)
+    public bool isMoving = false; //makes sure that the villain can only move up when its not already moving (avoid super fast villain bug)
+    public bool isSwitching = false;
 
-    static public int track = 1; //collision purposes (trolley has no volume, just needs to be in same track to hit a guy)
+    static public int vtrack = 1; //collision purposes (villain has no volume, just needs to be in same track to hit the trolley)
     void Start()
     {
-        forward_speed = 0.01f; //manually set speed (testing says this works nice, subject to change)
+        forward_speed = 0.005f; //manually set speed (testing says this works nice, subject to change)
     }
     // Update is called once per frames
     //Can't be delayed, need booleans like isMoving to properly implement delays
     //ex: without isMoving, StartCoroutine(moveBack()) could be called over and over and over and over (buggy)
     void Update()
     {
-        //makes sure that it can only move if it isn't moving already
-        if (!isMoving) {
+        //makes sure that it can only move if it isn't moving already (avoid super fast villain bug)
+        //can't move left/right and switch tracks at the same time
+        if (!isMoving && !isSwitching) {
             //StartCoroutine calls move
+            StartCoroutine(move());
+        }
+        if (!isMoving && !isSwitching) {
             StartCoroutine(move_tracks());
         }
-        //putting this seperate allows it to speed and switch at the same time
-        if (!isSpeeding) {
-            StartCoroutine(speed());
-        }
     }
-    //for speeding up to the right
-    IEnumerator speed() {
+    //for moving left and right 
+    IEnumerator move() {
         //bounds: -4.79 < x < 4.79
-        isSpeeding = true; 
-        while(Input.GetKey(KeyCode.D) && transform.position.x < 4.79) {
+        exp = 1.1f;
+        isMoving = true; 
+        while(Input.GetKey(KeyCode.RightArrow) && transform.position.x < 4.79) {
             transform.position += exp * forward_speed * transform.right; 
             exp += 0.1f;
             yield return new WaitForSeconds(0.01f);
         }
+        
         exp = 1.1f;
-        isSpeeding = false;
-        while(transform.position.x > -4.79) {
-            transform.position -= 0.00005f * transform.right; 
-            if (Input.GetKey(KeyCode.D)) {
+        while(Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -4.79) {
+            transform.position -= exp * forward_speed * transform.right; 
+            exp += 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        while(transform.position.x > 4.79) {
+            transform.position -= 0.0005f * transform.right; 
+            if(Input.GetKey(KeyCode.LeftArrow)) {
                 break;
             }
             yield return new WaitForSeconds(0.01f);
         }
+
+        while(transform.position.x < -4.79) {
+            transform.position += 0.0005f * transform.right; 
+            if(Input.GetKey(KeyCode.RightArrow)) {
+                break;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        isMoving = false;
     }
 
-    //switching tracks
     IEnumerator move_tracks()
         {
-            isMoving = true;
+            isSwitching = true;
             float scale = 0.25f;
 
-            //go "up" if press W
-            while(Input.GetKey(KeyCode.W)) {
+            //go "up" if press up arrow
+            while(Input.GetKey(KeyCode.UpArrow)) {
                 //bounds on how much it can switch (only 4 tracks)
                 //for loops let it increment its location (animation but real time)
-                if (track == 4) {
+                if (vtrack == 4) {
                     //wiggles basically, can add a sound effect later
                     for (float i = 0.01f; i <= 0.25f; i += 0.01f) {
                         transform.position += scale * transform.forward * (0.5f*i);
@@ -79,22 +94,17 @@ public class Trolley_Move : MonoBehaviour
                 }
                 else {
                     for (float i = 0.01f; i <= 0.25f; i += 0.01f) {
-                        transform.position += scale * transform.forward * (3.0f*i);
+                        transform.position += scale * transform.forward * (2.5f*i);
                         yield return new WaitForSeconds(0.01f);
                     }
-                    track++;
-                    for (float i = 0.01f; i <= 0.25f; i += 0.01f) {
-                        transform.position += -1.0f * scale * transform.forward * (0.5f*i);
-                        yield return new WaitForSeconds(0.01f);
-                    }
+                    vtrack++;
                 }
             }
 
-            //go "down" if press S
-            //same code basically
-            while(Input.GetKey(KeyCode.S)) {
+            //go "down" if press down arrow
+            while(Input.GetKey(KeyCode.DownArrow)) {
                 
-                if (track == 1) {
+                if (vtrack == 1) {
                     for (float i = 0.01f; i <= 0.25f; i += 0.01f) {
                         transform.position -= scale * transform.forward * (0.5f*i);
                         yield return new WaitForSeconds(0.01f);
@@ -110,18 +120,14 @@ public class Trolley_Move : MonoBehaviour
                 }
                 else {
                     for (float i = 0.01f; i <= 0.25f; i += 0.01f) {
-                        transform.position -= scale * transform.forward * (3.0f*i);
+                        transform.position -= scale * transform.forward * (2.5f*i);
                         yield return new WaitForSeconds(0.01f);
                     }
-                    track--;
-                    for (float i = 0.01f; i <= 0.25f; i += 0.01f) {
-                        transform.position -= -1.0f * scale * transform.forward * (0.5f*i);
-                        yield return new WaitForSeconds(0.01f);
-                    }
+                    vtrack--;
                 }
                 
             }
-            isMoving =  false;
+            isSwitching =  false;
         }
     
 }
