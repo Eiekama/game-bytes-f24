3fff494dbd2400a6a3141d4ab27970a643f4d8f9
Bossfire964 <74617357+Bossfire964@users.noreply.github.com>
2024-10-20 16:19:43 -0400

    Second Player Works

--- Crucible/Assets/Minigames/cs-get-degrees/Scripts/PlayerController.cs
+++ Crucible/Assets/Minigames/cs-get-degrees/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     //[SerializeField] GameObject player;
     [SerializeField] float speed = 1;
+    [SerializeField] int playerNum;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +19,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //print("I am running :)");
-        float horizontal = Input.GetAxis("P1_Horizontal");
+        float horizontal = Input.GetAxis(playerNum == 1 ? "P1_Horizontal": "P2_Horizontal");
         //print(horizontal);
-        if (horizontal > 0)
+        if (horizontal > 0 && transform.localPosition.x < (playerNum == 1 ? -3.66: 2.6))
         {
             transform.position += transform.right * speed * Time.deltaTime;
         }
-        else if (horizontal < 0)
+        else if (horizontal < 0 && transform.localPosition.x > (playerNum == 1 ? -9.1:-2.33))
         {
             //Friend friend = new Friend(gameObject);
             //friend.SpawnFriend();
             transform.position += -transform.right * speed * Time.deltaTime;
         }
+
+
     }
 }
