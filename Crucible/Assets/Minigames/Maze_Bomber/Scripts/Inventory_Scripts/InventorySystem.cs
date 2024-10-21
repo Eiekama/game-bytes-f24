2f0ae8dad949960363560b7f5db6aaf9bd28637d
Angie <angela.yanqizhu@gmail.com>
2024-10-20 20:32:04 -0400

    Finished (?) Backend for Inventory

--- Crucible/Assets/Minigames/Maze_Bomber/Scripts/Inventory_Scripts/InventorySystem.cs
+++ Crucible/Assets/Minigames/Maze_Bomber/Scripts/Inventory_Scripts/InventorySystem.cs
@@ -28,7 +28,42 @@ public class InventorySystem
 
     public bool AddToInventory(InventoryItemData itemToAdd, int amountToAdd)
     {
-        inventorySlots[0] = new InventorySlot(itemToAdd, amountToAdd);
-        return true;
+        if (ContainsItem(itemToAdd, out List<InventorySlot> invSlot)) // check whether item exists in inventory
+        {   // get a list of all slots that have the item in it
+            // and check for a slot that has room in the stack
+            // if stack is full, check next
+            // if no such slot, return false
+            foreach (var slot in invSlot)
+            {
+                if(slot.RoomLeftInStack(amountToAdd))
+                {
+                    slot.AddToStack(amountToAdd);
+                    OnInventorySlotChanged?.Invoke(slot);
+                    return true;
+                }
+            }
+        }
+        if (HasFreeSlot(out InventorySlot freeSlot)) // Gets the first available slot
+        {
+            freeSlot.UpdateInventorySlot(itemToAdd, amountToAdd);
+            OnInventorySlotChanged?.Invoke(freeSlot);
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool ContainsItem(InventoryItemData itemToAdd, out List<InventorySlot> invSlot) 
+    {
+        invSlot = InventorySlots.Where(i => i.ItemData == itemToAdd).ToList(); // using System.Linq
+        // Check all inventoryslots and create a list, fill it where our itemdata = itemToAdd, and put that in the list (invSlot)
+        // Debug.Log(invSlot.Count);
+        return invSlot == null ? false : true; // whether it contains the item
+    }
+
+    public bool HasFreeSlot(out InventorySlot freeSlot)
+    {
+        freeSlot = InventorySlots.FirstOrDefault(i => i.ItemData == null); // try to find an empty slot
+        return freeSlot == null ? false : true;
     }
 }
